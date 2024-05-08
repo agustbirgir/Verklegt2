0b9fe45736d2e7ffbd4d444c4b272fe6d4f69025
agustbirgir <agustbirgirkarlsson@gmail.com>
2024-05-08 11:34:05 +0000

    change database

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-"""
+
 DATABASES = { 'default':
                   { 'ENGINE': 'django.db.backends.postgresql',
                     'NAME': 'postgres',
@@ -97,7 +97,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
