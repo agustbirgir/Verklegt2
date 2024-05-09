ce5a392b201836adf808effdfe47ae7601129081
agustbirgir <agustbirgirkarlsson@gmail.com>
2024-05-09 16:04:09 +0000

    added a signup for company

--- Company/urls.py
+++ Company/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import  views
-
+from django.conf import settings
+from django.conf.urls.static import static
 """register the new urls here"""
 urlpatterns = [
     path('', views.index, name="index"),
     path('sign_up/', views.company_signup, name="company_signup"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Company/views.py
+++ Company/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Company
+from django.contrib.auth.hashers import make_password
 
 def index(request):
     return render(request, 'Company/index.html')
 
 def company_signup(request):
-    if request.method == 'POST':
-        pass
-    return render(request, 'Company/sign_up.html')
+    if request.method == "POST":
+        company_name = request.POST.get('company_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        about_company = request.POST.get('about_company')
+        company_image = request.FILES.get('company_image')
+        cover_image = request.FILES.get('cover_image')
+
+        if Company.objects.filter(email=email).exists():
+            return render(request, 'Company/sign_up.html', {'error': 'Email already in use'})
+
+        new_company = Company.objects.create_company(
+            email=email,
+            password=password,
+            company_name=company_name,
+            address=address,
+            about_company=about_company,
+            company_image=company_image,
+            cover_image=cover_image
+        )
+
+        print("new company created: ", new_company)
+        return redirect('login')
+
+    else:
+        return render(request, 'Company/sign_up.html')
